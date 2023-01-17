a3115679ba1099c0b1d9e6c49254612929f0872d
Anastasiya <nastja_1998-12@mail.ru>
2023-01-17 21:48:41 +0300

    done first page

--- script/script.js
+++ script/script.js
@@ -1,46 +1,43 @@
-function slider (){
-    const image = document.querySelectorAll('.slider .slider-line img');
-    const sliderLine = document.querySelector('.slider-line');
-    let count = 0;
-    let width;
+const image = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider-line');
+let count = 0;
+let width;
 
-    function initi(){
+function initi(){
 
-        width = document.querySelector('.slider-line').offsetWidth;
-        sliderLine.getElementsByClassName.width = width*image.length + 'px';
-        image.forEach(item => {
-            item.style.width = width + 'px';
-            item.style.height = 'auto';
-        });
-
-        rollSlider();
-    };
-
-    window.addEventListener('resize', initi);
-    initi();
-
-    document.querySelector('.slider-prev').addEventListener('click', function(){
-
-        count--;
-        if (count < 0 ){
-            count = image.length -1;
-        }
-        rollSlider();
-
+    width = document.querySelector('.slider-line').offsetWidth;
+    sliderLine.getElementsByClassName.width = width*image.length + 'px';
+    image.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
     });
 
-    document.querySelector('.slider-next').addEventListener('click', function(){
+    rollSlider();
+};
 
-        count++;
-        if (count >= image.length){
-            count = 0;
-        }
-        rollSlider();
+window.addEventListener('resize', initi);
+initi();
 
-    });
+document.querySelector('.slider-prev').addEventListener('click', function(){
 
-    function rollSlider(){
-        sliderLine.style.transform = 'translate(-' + count * width + 'px)';
-    };
+    count--;
+    if (count < 0 ){
+        count = image.length -1;
+    }
+    rollSlider();
 
+});
+
+document.querySelector('.slider-next').addEventListener('click', function(){
+
+    count++;
+    if (count >= image.length){
+        count = 0;
+    }
+    rollSlider();
+
+});
+
+function rollSlider(){
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
 };
